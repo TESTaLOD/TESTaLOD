21f61e829c099cde29d3e06894903a823d550f08
Luigi Asprino <luigi.asprino@gmail.com>
2020-03-30 10:13:54 +0200

    start update

--- inferenceServices/docker/start.sh
+++ inferenceServices/docker/start.sh
@@ -1,13 +1,9 @@
 cd /opt/TESTaLOD/inferenceServices
 
-echo $0
-
-echo $1
-
-echo $2
-
 echo "serverPort=$1" > src/main/resources/config.properties
 
 cat src/main/resources/config.properties
+
+mvn clean install
 
 mvn exec:java -Dexec.mainClass="it.cnr.istc.stlab.testalod.TestaLODServer"
