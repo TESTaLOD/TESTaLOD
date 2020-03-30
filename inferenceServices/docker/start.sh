085ab9ec4201d374e508a94ab1e41b14139a9911
Luigi Asprino <luigi.asprino@gmail.com>
2020-03-30 09:26:31 +0200

    port argument

--- inferenceServices/docker/start.sh
+++ inferenceServices/docker/start.sh
@@ -1,2 +1,7 @@
 cd /opt/TESTaLOD/inferenceServices
+
+echo "serverPort=$2" > src/main/resources/config.properties
+
+cat src/main/resources/config.properties
+
 mvn exec:java -Dexec.mainClass="it.cnr.istc.stlab.testalod.TestaLODServer"
