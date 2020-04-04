3800f3c0a916e7751a5c08aea33b74ea182589b5
Luigi Asprino <luigi.asprino@gmail.com>
2020-04-04 18:51:08 +0200

    integrity constraint check

---
+++ inferenceServices/src/main/java/it/cnr/istc/stlab/testalod/resources/IntegrityConstraintCheck.java
@@ -0,0 +1,62 @@
+package it.cnr.istc.stlab.testalod.resources;
+
+import java.io.ByteArrayOutputStream;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+@Path("/integrityConstraintCheck")
+public class IntegrityConstraintCheck {
+
+	private static final Logger logger = LogManager.getLogger(IntegrityConstraintCheck.class);
+
+	@GET
+	@Produces("application/rdf+xml")
+	public Response consistencyCheck(@QueryParam("IRI") String iri) {
+
+		logger.trace("Checking consistency of " + iri);
+
+		Model m;
+		try {
+			if (Utils.checkConsistency(iri)) {
+				m = getTrue(iri);
+			} else {
+				m = getFalse(iri);
+			}
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			m.write(bos, "RDF/XML");
+			return Response.ok(bos.toString()).build();
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
+		return Response.serverError().build();
+	}
+
+	private static Model getFalse(String iri) {
+		Model m = ModelFactory.createDefaultModel();
+		m.addLiteral(m.createResource(iri),
+				m.createProperty(
+						"http://www.ontologydesignpatterns.org/schemas/testannotationschema.owl#hasActualResult"),
+				false);
+		return m;
+	}
+
+	private static Model getTrue(String iri) {
+		Model m = ModelFactory.createDefaultModel();
+		m.addLiteral(m.createResource(iri),
+				m.createProperty(
+						"http://www.ontologydesignpatterns.org/schemas/testannotationschema.owl#hasActualResult"),
+				true);
+		return m;
+	}
+
+}
--- inferenceServices/src/main/java/it/cnr/istc/stlab/testalod/resources/Utils.java
+++ inferenceServices/src/main/java/it/cnr/istc/stlab/testalod/resources/Utils.java
@@ -53,11 +53,49 @@ public class Utils {
 		return reasoner.isConsistent();
 
 	}
+	
+	
+	static boolean checkIntegrityConstraint(String iriTestCase) throws OWLOntologyCreationException {
+
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = manager.loadOntology(IRI.create(iriTestCase.toString()));
+
+		ontology.importsClosure().forEach(ont -> {
+			logger.trace("Importing " + ont.getOntologyID().getOntologyIRI().toString());
+			ontology.addAxioms(ont.axioms());
+		});
+
+		OWLDataFactory factory = manager.getOWLDataFactory();
+
+		ontology.annotations().forEach(a -> {
+			if (a.containsEntityInSignature(factory.getOWLAnnotationProperty(
+					"http://www.ontologydesignpatterns.org/schemas/testannotationschema.owl#hasInputTestData"))) {
+				try {
+					logger.trace("Importing " + a.getValue().toString());
+					OWLOntology toyDataset = manager.loadOntology(IRI.create(a.getValue().toString()));
+					toyDataset.importsClosure().forEach(ont -> {
+						logger.trace("Importing " + ont.getOntologyID().getOntologyIRI().toString());
+						ontology.addAxioms(ont.axioms());
+					});
+					ontology.addAxioms(toyDataset.axioms());
+				} catch (OWLOntologyCreationException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		ConsoleProgressMonitor progressMonitor = new ConsoleProgressMonitor();
+		OWLReasonerConfiguration config = new SimpleConfiguration(progressMonitor);
+		OWLReasoner reasoner = new org.semanticweb.HermiT.ReasonerFactory().createReasoner(ontology, config);
+
+		return reasoner.isConsistent();
+
+	}
 
 	public static void main(String[] args) throws OWLOntologyCreationException {
 //		checkConsistency("file:///Users/lgu/Dropbox/repository/workspace/arco-git/ArCo-release/test/VI/testcase-01.owl");
-		checkConsistency(
-				"https://raw.githubusercontent.com/ICCD-MiBACT/ArCo/master/ArCo-release/test/VI/testcase-01.owl");
+		checkIntegrityConstraint(
+				"https://raw.githubusercontent.com/ICCD-MiBACT/ArCo/master/ArCo-release/test/IC/testcase-01.owl");
 	}
 
 }
